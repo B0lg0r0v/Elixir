5e52ed1ef39b2611728d60e7d26203bc68c559a7
B0lg0r0v <115954804+B0lg0r0v@users.noreply.github.com>
2024-07-26 15:27:13 +0200

    v1.0.0
    Refer to release section for the changelog

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+setup(
+    name='elixir',
+    version='1.0.0',
+    description='Elixir is a fast multi-function DNS Enumeration, Subdomain Enumeration and Attack Surface Mapping tool.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/B0lg0r0v/Elixir',
+    author='B0lg0r0v',
+    author_email='contact@arthurminasyan.com',
+    maintainer='B0lg0r0v',
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4',
+        'dnspython',
+        'requests',
+    ],
+    packages=[
+        'src',
+        'src.core',
+    ],
+    entry_points={
+        'console_scripts': [
+            'elixir=src.entry:main',
+        ],
+
+    },
+    platforms=['Unix'],    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    
+    ]   
+
+)
--- src/core/dnsfunc.py
+++ src/core/dnsfunc.py
@@ -1,86 +1,286 @@
+from src.core.colors import Color
 import dns.resolver
-import dns.zone
 import dns.reversename
-from colorama import init as coloramaInit
-from colorama import Fore
+from bs4 import BeautifulSoup
+import requests
+import json
 
-coloramaInit(autoreset=True)
+class DnsEnum:
 
-#----------------- Record Enumeration -----------------#
+    def __init__(self, domain):
+        if domain is not None:
+            self.domain = domain
+        
+        self.colors = Color()
+        self.resolver = dns.resolver.Resolver()
 
-class dnsEnumeration:
+    def get_ip(self) -> str:
+        try:
+            ip = self.resolver.resolve(self.domain, 'A')
+            for rdata in ip:
+                return rdata
+        except Exception as e:
+            return None
 
-    def findAllDnsRecords(domain):
-        dnsRecordTypes = ['A', 'AAAA', 'NS', 'CNAME', 'TXT', 'SOA', 'PTR', 'MX', 'SRV']
-        server = []
-        serverOutput = []
+
+    # Get all DNS records for the domain. The OS resolver is used.
+    def get_all_dns_records(self):
+        dns_records = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'TXT']
+        results = []
+
+        print(self.colors.yellow(f'\n[+] All DNS records for {self.domain}:') + '\n')
+        results.append(f'\nAll DNS records for {self.domain}:\n')
 
         try:
-            for dnsRecords in dnsRecordTypes:
+            for records in dns_records:
                 try:
-                    resolve = dns.resolver.resolve(domain, dnsRecords)
-                    for answers in resolve:
-                        server.append(f'{Fore.GREEN}{dnsRecords}: ' + answers.to_text() + Fore.RESET + '\n')
-                        serverOutput.append(f'{dnsRecords}: {answers.to_text()}\n')
+                    resolve = self.resolver.resolve(self.domain, records)
+                    for rdata in resolve:
+                        print(self.colors.green(f'{records}: {rdata}'))
+                        results.append(f'{records}: {rdata}')
+
                 except dns.resolver.NoAnswer:
-                    server.append(f'{Fore.RED}{dnsRecords}: Record not existing{Fore.RESET}\n')
-        except dns.resolver.NXDOMAIN:
-            print(f'{Fore.RED}{domain} does not exist.{Fore.RESET}\n')
-        except (dns.resolver.NoResolverConfiguration, dns.resolver.LifetimeTimeout):
-            print(f'{Fore.RED}No NS found or no internet connection.{Fore.RESET}')
+                    print(self.colors.red(f'No {records} records found.'))   
 
-        print(Fore.GREEN + ''.join(server) + Fore.RESET)
-        
-        return serverOutput
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+            
+        return results
+    
+    # Get specific DNS records for the domain. The OS resolver is used.
+    def get_specific_dns_records(self, records):
+        print(self.colors.yellow(f'\n[+] Specific DNS records for {self.domain}:') + '\n')
 
-
-    def findSpecificRecord(domain, record):
-        response = []
-        responseOutput = []
-        
         try:
-            for recordType in record:
+            for record in records:
                 try:
-                    resolve = dns.resolver.resolve(domain, recordType)
-                    for answers in resolve:
-                        response.append(f'{Fore.GREEN}{recordType} Record: {answers.to_text()}{Fore.RESET}\n')
-                        responseOutput.append(f'{recordType} Record: {answers.to_text()}\n')
+                    resolve = self.resolver.resolve(self.domain, record)
+                    for rdata in resolve:
+                        print(self.colors.green(f'{record}: {rdata}'))
+
                 except dns.resolver.NoAnswer:
-                    response.append(f'{Fore.RED}{recordType} Record not existing.{Fore.RESET}\n')
-                    responseOutput.append(f'{recordType} Record not existing.\n')
+                    print(self.colors.red(f'No {record} records found.'))   
 
-        except dns.resolver.NXDOMAIN:
-            print(f'{Fore.RED}{domain} does not existing.{Fore.RESET}')
-        except dns.rdatatype.UnknownRdatatype:
-            print(f'{Fore.RED}Error in your record statement.{Fore.RESET}')
-        except dns.resolver.NoResolverConfiguration:
-            print(f'{Fore.RED}No NS found or no internet connection.{Fore.RESET}')
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
 
+
+    # Reverse DNS lookup with IP address. Using the OS resolver.
+    def reverse_dns_lookup(self, ip):
+        print(self.colors.yellow(f'\n[+] Reverse DNS Lookup for {ip}:') + '\n')
+
+        try:
+            rev_name = dns.reversename.from_address(ip)
+            resolve = self.resolver.resolve(rev_name, 'PTR')
+            for rdata in resolve:
+                print(self.colors.green(f'PTR: {rdata}'))
+
+        except dns.resolver.NoAnswer:
+            print(self.colors.red(f'No PTR records found.'))   
+
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+
+
+    def map_attack_surface(self):
+        print(self.colors.yellow(f'\n[+] Mapping the attack surface for {self.domain}:') + '\n')
+
+        indent = '    '  
+        dns_records = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'TXT']
+
+        print(self.colors.yellow(f'┌──———————— {self.domain}————————'))
+        try:
+            for records in dns_records:
+                try:
+                    resolve = self.resolver.resolve(self.domain, records)
+                    for rdata in resolve:
+                        subdomain = rdata.to_text()
+                        #print(self.colors.green(f'{indent}| {records}: {subdomain}'))
+                        if records == 'A' or records == 'AAAA':
+                            
+                            print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'{self.colors.blue(self.geolocation(subdomain))}')
+                        
+                        if records == 'MX':
+                            mx = subdomain.split(' ')[1]
+                            mxIP = self.resolver.resolve(mx, 'A')
+                            for ip in mxIP:
+                                print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'IP: {ip}')
+
+                        if records == 'NS':
+                            nsIP = self.resolver.resolve(subdomain, 'A')
+                            for ip in nsIP:
+                                print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'IP: {ip}')
+                                
+                except dns.resolver.NoAnswer:
+                    pass
         
-        print(Fore.GREEN + ''.join(response) + Fore.RESET)
+        except KeyboardInterrupt:
+            pass
+
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+    
+    def geolocation(self, subdomain):
         
-        return responseOutput
+        response = requests.get(f'http://ip-api.com/json/{subdomain}') #Using http instead of https because of the free API. You may want to change that.
+        data = response.json()
+    
+        if response.status_code == 200:
+            city = data.get('city')
+            region = data.get('regionName')
+            country = data.get('country')
+            isp = data.get('isp')
+            asn = data.get('as')
+
+            res = f'City: {city}, Region: {region}, Country: {country}, ISP: {isp}, ASN: {asn}'
+            return res
+        else:
+            print(f'Error: {data.get("message")}')
+        
 
 
-    def reverseLookup(ipAddress):
-        dnsNames = []
-        dnsNamesOutput = []
+    # Zone transfer.
+    def zone_transfer(self):
+        print(self.colors.yellow(f'\n[+] Attempting Zone Transfer for {self.domain}:') + '\n')
+
+        try:
+            ns = self.resolver.resolve(self.domain, 'NS')
+            for rdata in ns:
+                ip = self.resolver.resolve(rdata.to_text(), 'A')
+                for ips in ip:
+                    try:
+                        zone = dns.zone.from_xfr(dns.query.xfr(str(ips), self.domain))
+                        for host in zone:
+                            print(self.colors.green(f'{host}'))
+
+                    except dns.zone.NoAxfr:
+                        print(self.colors.red(f'Zone Transfer failed.'))
+        
+        except dns.resolver.NoAnswer:
+            print(self.colors.red(f'No NS records found.'))
+        
+        except Exception as e:
+            print(self.colors.red(f'Error: Zone Transfer refused.'))
+
+    
+    def get_asn(self):
+        print(self.colors.yellow(f'\n[+] ASN information for {self.domain}:') + '\n')
+
+        hosts = []
+
+        url = f'https://urlscan.io/api/v1/search/?q={self.domain}'
+        soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+        data = json.loads(soup.text)
+
+        for item in data['results']:
+            if self.domain in item['page']['apexDomain']:
+                asn = item['page'].get('asn')
+                domain = item['page'].get('domain')
+                asn_name = item['page'].get('asnname')
+                if asn and asn not in hosts:
+                    hosts.append(asn)
+                    print(self.colors.green(f'ASN: {asn} - {asn_name} - ({domain})'))
+        
+        return hosts
+
+
+
+
+    #------------------ Custom DNS resolver ------------------#
+
+    # Get all DNS records for the domain. A custom DNS resolver is used.
+    def get_all_dns_records_resolver(self, resolver):
+        dns_records = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'TXT']
+        self.resolver.nameservers = resolver
+        
+        print(self.colors.yellow(f'\n[+] All DNS records for {self.domain}:') + '\n')
+
+        try:
+            for records in dns_records:
+                try:
+                    resolve = self.resolver.resolve(self.domain, records)
+                    for rdata in resolve:
+                        print(self.colors.green(f'{records}: {rdata}'))
+
+                except dns.resolver.NoAnswer:
+                    print(self.colors.red(f'No {records} records found.'))   
+
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+
+    
+    # Get specific DNS records for the domain. A custom DNS resolver is used.
+    def get_specific_dns_records_resolver(self, resolver, records):
+        self.resolver.nameservers = resolver
+
+        print(self.colors.yellow(f'\n[+] Specific DNS records for {self.domain}:') + '\n')
         
         try:
-            for ips in ipAddress:
-                names = dns.reversename.from_address(ips)
-                dnsNames.append(f'{Fore.GREEN}Reverse Lookup: {str(dns.resolver.resolve(names, "PTR")[0])}{Fore.RESET}\n')
-                dnsNamesOutput.append(f'Reverse DNS Lookup: {str(dns.resolver.resolve(names, "PTR")[0])}\n')
-        except dns.resolver.NXDOMAIN:
-            print(f'{Fore.RED}{names} does not existing.{Fore.RESET}')
-            dnsNamesOutput.append(f'{names} does not existing.\n')
+            for record in records:
+                try:
+                    resolve = self.resolver.resolve(self.domain, record)
+                    for rdata in resolve:
+                        print(self.colors.green(f'{record}: {rdata}'))
 
+                except dns.resolver.NoAnswer:
+                    print(self.colors.red(f'No {record} records found.'))   
+
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+
+    def reverse_dns_lookup_ns(self, ip, resolver):
+        self.resolver.nameservers = resolver
+
+        print(self.colors.yellow(f'\n[+] Reverse DNS Lookup for {ip}:') + '\n')
+
+        try:
+            rev_name = dns.reversename.from_address(ip)
+            resolve = self.resolver.resolve(rev_name, 'PTR')
+            for rdata in resolve:
+                print(self.colors.green(f'PTR: {rdata}'))
+
+        except dns.resolver.NoAnswer:
+            print(self.colors.red(f'No PTR records found.'))   
+
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
+    
+    def map_attack_surface_ns(self, resolver):
+        self.resolver.nameservers = resolver
+
+        print(self.colors.yellow(f'\n[+] Mapping the attack surface for {self.domain}:') + '\n')
+
+        indent = '    '  
+        dns_records = ['A', 'AAAA', 'CNAME', 'MX', 'NS', 'PTR', 'SOA', 'TXT']
+
+        print(self.colors.yellow(f'┌──———————— {self.domain}————————'))
+        try:
+            for records in dns_records:
+                try:
+                    resolve = self.resolver.resolve(self.domain, records)
+                    for rdata in resolve:
+                        subdomain = rdata.to_text()
+                        #print(self.colors.green(f'{indent}| {records}: {subdomain}'))
+                        if records == 'A' or records == 'AAAA':
+                            
+                            print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'{self.colors.blue(self.geolocation(subdomain))}')
+                        
+                        if records == 'MX':
+                            mx = subdomain.split(' ')[1]
+                            mxIP = self.resolver.resolve(mx, 'A')
+                            for ip in mxIP:
+                                print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'IP: {ip}')
+
+                        if records == 'NS':
+                            nsIP = self.resolver.resolve(subdomain, 'A')
+                            for ip in nsIP:
+                                print(self.colors.yellow(f'│ ') + self.colors.green(f'{records}: {subdomain}') + ' -----> ' + f'IP: {ip}')
+                                
+                except dns.resolver.NoAnswer:
+                    pass
         
-        print(''.join(dnsNames))
+        except KeyboardInterrupt:
+            pass
 
-        return dnsNamesOutput
-
-#---------------------------------------------------------# 
-
-
-
+        except Exception as e:
+            print(self.colors.red(f'Error: {type(e).__name__}'))
